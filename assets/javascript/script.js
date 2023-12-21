d7198cc25eff7f15ada6e791b9328c4e87ccd0f2
DannyGilligan <gilligan.danny@gmail.com>
2023-12-21 23:35:01 +0000

    Add innertext change rules to handleusernamesubmit method

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,19 @@
 
 
 let quizEngine = {
+
+    /* Object Properties */
     userName: null,
     mainButton: document.getElementById("main-button"),
 
 
+    /* Object Methods */
     handleUserNameSubmit: function (event) {
         event.preventDefault();
         quizEngine.userName = document.getElementById('username-input');
+        document.getElementById('top-panel-inner').innerText = `Ok ${quizEngine.userName.value}, let's get the rules out of the way..`
         console.log(quizEngine.userName.value);
+        document.getElementById('main-button').innerText = 'Kick Off!';
         document.getElementById('enter-username-container').style.display = 'none';
         document.getElementById('quiz-rules-inner-container').style.display = 'flex';
     }
