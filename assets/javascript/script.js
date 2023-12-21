ff28a549583a518248be770fde25dad42ac657d6
DannyGilligan <gilligan.danny@gmail.com>
2023-12-21 23:19:29 +0000

    Add handleUserNameSubmit method to quizEngine object

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,5 +3,20 @@
 let quizEngine = {
     userName: null,
     mainButton: document.getElementById("main-button"),
+
+
+    handleUserNameSubmit: function (event) {
+        event.preventDefault();
+        quizEngine.userName = document.getElementById('username-input');
+        console.log(quizEngine.userName.value);
+    }
 }
+
+let enterUsernameForm = document.getElementById('enter-username')
+enterUsernameForm.addEventListener("submit", quizEngine.handleUserNameSubmit)
+
+
+/*
+let user = document.getElementById('username-input');
+*/
 
