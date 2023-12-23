350d8c697c503334d353aebb937daced6c1ed3f2
DannyGilligan <gilligan.danny@gmail.com>
2023-12-23 02:30:44 +0000

    Add choiceContainers property and values to quizEngine object

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -28,31 +28,39 @@ let quizEngine = {
     welcomeMessage: `Welcome to the FIFA 2023 Women's World Cup Quiz`,
     questionCounter: 0,
     questions: [
-        'Is this question 1?', // Text for Question 1
-        'Is this question 2?', // Text for Question 2
-        'Is this question 3?', // Text for Question 3
-        'Is this question 4?', // Text for Question 4
-        'Is this question 5?', // Text for Question 5
-        'Is this question 6?', // Text for Question 6
-        'Is this question 7?', // Text for Question 7
-        'Is this question 8?', // Text for Question 8
-        'Is this question 9?', // Text for Question 9
+        'Is this question 1?',  // Text for Question 1
+        'Is this question 2?',  // Text for Question 2
+        'Is this question 3?',  // Text for Question 3
+        'Is this question 4?',  // Text for Question 4
+        'Is this question 5?',  // Text for Question 5
+        'Is this question 6?',  // Text for Question 6
+        'Is this question 7?',  // Text for Question 7
+        'Is this question 8?',  // Text for Question 8
+        'Is this question 9?',  // Text for Question 9
         'Is this question 10?', // Text for Question 10
         'Is this question 11?'  // Text for Question 11
     ],
     choices: [
-        ['Q1 Choice 1', 'Q1 Choice 2 correct', 'Q1 Choice 3', 'Q1 Choice 4'], // Choices for Question 1
-        ['Q2 Choice 1', 'Q2 Choice 2', 'Q2 Choice 3', 'Q2 Choice 4 correct'], // Choices for Question 2
-        ['Q3 Choice 1', 'Q3 Choice 2', 'Q3 Choice 3 correct', 'Q3 Choice 4'], // Choices for Question 3
-        ['Q4 Choice 1', 'Q4 Choice 2 correct', 'Q4 Choice 3', 'Q4 Choice 4'], // Choices for Question 4
-        ['Q5 Choice 1 correct', 'Q5 Choice 2', 'Q5 Choice 3', 'Q5 Choice 4'], // Choices for Question 5
-        ['Q6 Choice 1', 'Q6 Choice 2', 'Q6 Choice 3 correct', 'Q6 Choice 4'], // Choices for Question 6
-        ['Q7 Choice 1 correct', 'Q7 Choice 2', 'Q7 Choice 3', 'Q7 Choice 4'], // Choices for Question 7
-        ['Q8 Choice 1', 'Q8 Choice 2', 'Q8 Choice 3', 'Q8 Choice 4 correct'], // Choices for Question 8
-        ['Q9 Choice 1', 'Q9 Choice 2 correct', 'Q9 Choice 3', 'Q9 Choice 4'], // Choices for Question 9
+        ['Q1 Choice 1', 'Q1 Choice 2 correct', 'Q1 Choice 3', 'Q1 Choice 4'],     // Choices for Question 1
+        ['Q2 Choice 1', 'Q2 Choice 2', 'Q2 Choice 3', 'Q2 Choice 4 correct'],     // Choices for Question 2
+        ['Q3 Choice 1', 'Q3 Choice 2', 'Q3 Choice 3 correct', 'Q3 Choice 4'],     // Choices for Question 3
+        ['Q4 Choice 1', 'Q4 Choice 2 correct', 'Q4 Choice 3', 'Q4 Choice 4'],     // Choices for Question 4
+        ['Q5 Choice 1 correct', 'Q5 Choice 2', 'Q5 Choice 3', 'Q5 Choice 4'],     // Choices for Question 5
+        ['Q6 Choice 1', 'Q6 Choice 2', 'Q6 Choice 3 correct', 'Q6 Choice 4'],     // Choices for Question 6
+        ['Q7 Choice 1 correct', 'Q7 Choice 2', 'Q7 Choice 3', 'Q7 Choice 4'],     // Choices for Question 7
+        ['Q8 Choice 1', 'Q8 Choice 2', 'Q8 Choice 3', 'Q8 Choice 4 correct'],     // Choices for Question 8
+        ['Q9 Choice 1', 'Q9 Choice 2 correct', 'Q9 Choice 3', 'Q9 Choice 4'],     // Choices for Question 9
         ['Q10 Choice 1 correct', 'Q10 Choice 2', 'Q10 Choice 3', 'Q10 Choice 4'], // Choices for Question 10
         ['Q11 Choice 1', 'Q11 Choice 2', 'Q11 Choice 3 correct', 'Q11 Choice 4']  // Choices for Question 11
     ],
+
+    choiceContainers: [
+        choice1Container,
+        choice2Container,
+        choice3Container,
+        choice4Container
+    ],
+    
     answers: [
         'choice-2-input', // Answer for Question 1
         'choice-4-input', // Answer for Question 2
@@ -129,9 +137,9 @@ let quizEngine = {
      * tracker container, var assist container and goals scored container.
      */
     displayQuiz: function () {
-        /* mainButton.setAttribute('form', 'choices-container-outer') */
         document.getElementById('quiz-rules-inner-container').style.display = 'none';    // Hides the quiz rules screen
-        quizEngine.trackerItems[0].classList.add('tracker-item-active');                 // Adds active item class to tracker item, this should only run once  
+        quizEngine.trackerItems[quizEngine.questionCounter].classList.add('tracker-item-active');                // Adds active item class to tracker item, this should only run once
+        questionDialogueBox.innerText = quizEngine.questions[quizEngine.questionCounter];                         // Displays the question text to the user, driven by the questionCounter  
         choice1Container.innerText = quizEngine.choices[quizEngine.questionCounter][0];  // Displays choice 1 text to user
         choice2Container.innerText = quizEngine.choices[quizEngine.questionCounter][1];  // Displays choice 2 text to user
         choice3Container.innerText = quizEngine.choices[quizEngine.questionCounter][2];  // Displays choice 3 text to user
@@ -140,10 +148,8 @@ let quizEngine = {
         document.getElementById('tracker-container-inner').style.visibility = 'visible'; // Makes the tracker feature visible
         document.getElementById('bottom-panel-item1').style.visibility = 'visible';      // Makes the VAR Assist button visible
         document.getElementById('bottom-panel-item3').style.visibility = 'visible';      // Makes the Goals Scored box visible
-        questionDialogueBox.innerText = quizEngine.questions[0];                         // Displays the question text to the user, driven by the questionCounter
         mainButton.innerText = 'Shoot!';                                                 // Changes the text on the main button to 'Shoot!'
         mainButton.setAttribute('form', 'choices-container-outer');                      // Connects the main button to the user choice form to enable HTML validation
-        /*mainButton.addEventListener = ("submit", quizEngine.handleUserChoiceSubmit)    // Add an event listener to the main button, 'handing off' the functionality*/
     },
 
     /** 
@@ -161,34 +167,49 @@ let quizEngine = {
      * method.
      */
     handleUserChoiceSubmit: function (event) {
-        event.preventDefault();                                                                                 // prevents the form from submitting
+        event.preventDefault();                                                                                  // prevents the form from submitting
         
-        for (let i = 0; i < quizEngine.userChoiceInputs.length; i++) {                                          // For loop that will iterate over the user inputs
-            if (quizEngine.userChoiceInputs[i].checked === true) {                                              // An IF conditional determines which input has been checked
-                quizEngine.userChoiceSubmitted = quizEngine.userChoiceInputs[i].id;                             // The id of the checked input is then assigned to the userChoiceSubmitted property
-                quizEngine.currentCorrectAnswer = quizEngine.answers[quizEngine.questionCounter];               // Assigns the answer to the current question to the currentCorrectAnswer property
-                if (quizEngine.userChoiceSubmitted === quizEngine.currentCorrectAnswer) {                       // An IF conditional determines if the user choice and the correct answer match
-                    ++quizEngine.goalsScored;                                                                   // Increments goals scored if answer is correct
-                    goalsScoredDisplay.innerText = quizEngine.goalsScored;                                      // Displays the goals scored on screen to the user
+        for (let i = 0; i < quizEngine.userChoiceInputs.length; i++) {                                           // For loop that will iterate over the user inputs
+            if (quizEngine.userChoiceInputs[i].checked === true) {                                               // An IF conditional determines which input has been checked
+                quizEngine.userChoiceSubmitted = quizEngine.userChoiceInputs[i].id;                              // The id of the checked input is then assigned to the userChoiceSubmitted property
+                quizEngine.currentCorrectAnswer = quizEngine.answers[quizEngine.questionCounter];                // Assigns the answer to the current question to the currentCorrectAnswer property
+                if (quizEngine.userChoiceSubmitted === quizEngine.currentCorrectAnswer) {                        // An IF conditional determines if the user choice and the correct answer match
+                    ++quizEngine.goalsScored;                                                                    // Increments goals scored if answer is correct
+                    goalsScoredDisplay.innerText = quizEngine.goalsScored;                                       // Displays the goals scored on screen to the user
                     quizEngine.trackerItems[quizEngine.questionCounter].classList.remove('tracker-item-active'); // Removes tracker item active class
                     quizEngine.trackerItems[quizEngine.questionCounter].classList.add('tracker-item-correct');   // Adds tracker item correct class
-                    document.getElementById('answer-feedback-status').innerHTML = `<span>GOAL!</span>`;         // Updates the feedback text
-                    document.getElementById('choices-container-outer').style.display = 'none';                  // Hides the choices container
-                    document.getElementById('answer-feedback-outer').style.display = 'flex';                    // Displays the answer feedback screen
+                    document.getElementById('answer-feedback-status').innerHTML = `<span>GOAL!</span>`;          // Updates the feedback text
+                    document.getElementById('choices-container-outer').style.display = 'none';                   // Hides the choices container
+                    document.getElementById('answer-feedback-outer').style.display = 'flex';                     // Displays the answer feedback screen
                     document.getElementById('answer-feedback-image-container').innerHTML = `<img src="assets/images/goal_image_${quizEngine.questionCounter}.webp" alt="Female Footballer" id="answer-feedback-image">`; // Displays the 'GOAL!' feedback image
-                    mainButton.innerText = 'Play on!';                                                          // Changes text of main button to 'Play On!'
+                    mainButton.innerText = 'Play on!';                                                           // Changes text of main button to 'Play On!'
+                    mainButton.addEventListener('click', quizEngine.displayNextQuestion);
 
-                } else {                                                                                        // If the answer is incorrect, the code below runs
+                } else {                                                                                         // If the answer is incorrect, the code below runs
                     document.getElementById('answer-feedback-image-container').innerHTML = `<img src="assets/images/miss_image_${quizEngine.questionCounter}.webp" alt="Female Footballer" id="answer-feedback-image">`; // Displays the 'MISS!' feedback image
                     quizEngine.trackerItems[quizEngine.questionCounter].classList.remove('tracker-item-active'); // Removes tracker item active class
                     quizEngine.trackerItems[quizEngine.questionCounter].classList.add('tracker-item-incorrect'); // Adds tracker item incorrect class
-                    document.getElementById('answer-feedback-status').innerHTML = `<span>MISS!</span>`;         // Updates the feedback text
-                    document.getElementById('choices-container-outer').style.display = 'none';                  // Hides the choices container
-                    document.getElementById('answer-feedback-outer').style.display = 'flex';                    // Displays the feedback container to the user
-                    mainButton.innerText = 'Play on!';                                                          // Changes text of main button to 'Play On!'
+                    document.getElementById('answer-feedback-status').innerHTML = `<span>MISS!</span>`;          // Updates the feedback text
+                    document.getElementById('choices-container-outer').style.display = 'none';                   // Hides the choices container
+                    document.getElementById('answer-feedback-outer').style.display = 'flex';                     // Displays the feedback container to the user
+                    mainButton.innerText = 'Play on!';                                                           // Changes text of main button to 'Play On!'
+                    mainButton.addEventListener('click', quizEngine.displayNextQuestion);
                 }
             }
         }
+    },
+
+    displayNextQuestion: function () {
+        document.getElementById("choices-container-outer").reset();                                             // Resets the form, clearing the radio inputs (add credit to readme!)
+
+        questionDialogueBox.innerText = quizEngine.questions[quizEngine.questionCounter];
+        document.getElementById('answer-feedback-outer').style.display = 'none';                                // Hides the answer feedback screen
+        ++quizEngine.questionCounter;                                                                           // Increments questionCounter
+        document.getElementById('choices-container-outer').style.display = 'flex';                              // Displays choices container
+        quizEngine.trackerItems[quizEngine.questionCounter].classList.add('tracker-item-active');               // Adds tracker item active class
+        console.log(quizEngine.questionCounter);
+
+        for (let i = 0; i < )
     }
 };
 
@@ -199,19 +220,4 @@ let mainButton = document.getElementById('main-button');
 
 const submitUserChoiceForm = document.getElementById('choices-container-outer');
 submitUserChoiceForm.addEventListener("submit", quizEngine.handleUserChoiceSubmit);
-
-
-
-
-
-/*
-const choice1Container = document.getElementById('choice-1-text').innerText = quizEngine.choices[quizEngine.questionCounter][0];
-const choice2Container = document.getElementById('choice-2-text').innerText = quizEngine.choices[quizEngine.questionCounter][1];
-const choice3Container = document.getElementById('choice-3-text').innerText = quizEngine.choices[quizEngine.questionCounter][2];
-const choice4Container = document.getElementById('choice-4-text').innerText = quizEngine.choices[quizEngine.questionCounter][3];
-*/
-
-/*
-let user = document.getElementById('username-input');
-*/
 
