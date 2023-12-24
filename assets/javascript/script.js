c03fb4fa3a555c8b6eed0f1c5d91bbff46f059b9
DannyGilligan <gilligan.danny@gmail.com>
2023-12-24 16:13:03 +0000

    Change variable type to const for questionDialogueBox

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,7 +19,7 @@ const trackerItem8 = document.getElementById('tracker-item-7');                 
 const trackerItem9 = document.getElementById('tracker-item-8');                     // Individual tracker item shown in the tracker panel.
 const trackerItem10 = document.getElementById('tracker-item-9');                    // Individual tracker item shown in the tracker panel.
 const trackerItem11 = document.getElementById('tracker-item-10');                   // Individual tracker item shown in the tracker panel.
-let questionDialogueBox = document.getElementById('top-panel-inner');               // Displays the main dialogue to the user (question text etc).
+const questionDialogueBox = document.getElementById('top-panel-inner');               // Displays the main dialogue to the user (question text etc).
 let mainButton = document.getElementById('main-button');
 
 let quizEngine = {
