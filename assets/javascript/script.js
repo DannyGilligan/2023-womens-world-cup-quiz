4f1ab50f976cf60ce64a1edcf5d80fb0b4734727
DannyGilligan <gilligan.danny@gmail.com>
2023-12-24 06:01:41 +0000

    Add style class disabled

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -250,6 +250,9 @@ let quizEngine = {
         document.getElementById(quizEngine.varAssists[quizEngine.questionCounter][0]).style.visibility = 'hidden';
         document.getElementById(quizEngine.varAssists[quizEngine.questionCounter][1]).style.visibility = 'hidden';
         document.getElementById('var-assist-remaining').innerHTML = `<span>${quizEngine.varAssistCounter} Remaining</span>`;
+        document.getElementById('var-assist-button').disabled = true;
+        document.getElementById('var-assist-button').disabled.style.background = 'red';
+
     },
  
 
