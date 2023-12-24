93110db58ea74174dd5cb076d5e1b71da4fe5d43
DannyGilligan <gilligan.danny@gmail.com>
2023-12-24 18:27:16 +0000

    Add semicolon in text of end quiz message

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -278,7 +278,7 @@ const quizEngine = {
         questionDialogueBox.innerHTML = `Congratulations on making it to the final whistle ${quizEngine.userName.value}!`;
         document.getElementById('answer-feedback-outer').style.display = 'none';
         document.getElementById('end-quiz-message').style.display = 'flex';
-        document.getElementById('end-quiz-inner').innerHTML = `<p>You scored <strong>${quizEngine.goalsScored}</strong> out of <strong>11</strong> attempts on goal!<br><br>Learn more about the devlopment of women's football at<br><br><a href="https://www.fifa.com/womens-football" target="_blank"><strong>fifa.com/womens-football</strong></a></p>`
+        document.getElementById('end-quiz-inner').innerHTML = `<p>You scored <strong>${quizEngine.goalsScored}</strong> out of <strong>11</strong> attempts on goal!<br><br>Learn more about the devlopment of women's football at:<br><br><a href="https://www.fifa.com/womens-football" target="_blank"><strong>fifa.com/womens-football</strong></a></p>`
     },
 
     varAssist: function () {
